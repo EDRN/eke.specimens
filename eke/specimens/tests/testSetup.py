9ca18001ecbd514ccfb6427977ef32fe5f8a4cd3
Sean Kelly <sean.c.kelly@me.com>
2011-09-29 02:39:55 +0000

    remove obsolete tests

--- eke/specimens/tests/testSetup.py
+++ eke/specimens/tests/testSetup.py
@@ -18,33 +18,27 @@ class TestSetup(BaseTestCase):
         '''Make sure our types are available.'''
         types = getToolByName(self.portal, 'portal_types').objectIds()
         for i in (
-            'Specimen Folder', 'Specimens at Site in Protocol', 'Specimens in Protocol', 'Specimen Record'
+            'Specimen Collection Folder', 'Specimen Collection'
         ):
             self.failUnless(i in types)
-    def testTypesNotSearched(self):
-        '''Ensure our "structural" types aren't searched by default.'''
-        notSearched = self.portal.portal_properties.site_properties.getProperty('types_not_searched')
-        for i in ('Specimens in Protocol', 'Specimens at Site in Protocol'): # TODO: Both?
-            self.failUnless(i in notSearched)
     def testCatalogIndexes(self):
         '''Check if indexes are properly installed.'''
         catalog = getToolByName(self.portal, 'portal_catalog')
         indexes = catalog.indexes()
-        for i in ('specimenCount', 'participantCount', 'identifier', 'specimenType', 'storageType', 'cancerDiagnosis', 'siteName'):
+        for i in ('specimenCount',):
             self.failUnless(i in indexes)
     def testCatalogMetadata(self):
         '''Check if indexed metadata schema are properly installed.'''
         catalog = getToolByName(self.portal, 'portal_catalog')
         metadata = catalog.schema()
-        for i in ('specimenCount', 'participantCount', 'identifier'):
+        for i in ('specimenCount',):
             self.failUnless(i in metadata)
-    def testVocabularies(self):
-        '''Ensure our vocabularies are available'''
-        vocabs = (
-            SPECIMEN_TYPE_VOCAB_NAME, STORAGE_VOCAB_NAME, u'eke.specimens.CancerDiagnosisVocabulary', u'eke.specimens.ERNESites'
-        )
-        for v in vocabs:
-            self.failUnless(queryUtility(IVocabularyFactory, name=v) is not None)
+    def testAddons(self):
+        '''Check that dependent packages are installed'''
+        qi = getToolByName(self.portal, 'portal_quickinstaller')
+        # TODO: When we migrate from Archetypes to Dexterity, check for plone.app.dexterity
+        # self.failUnless(qi.isProductInstalled('plone.app.dexterity'), "Dexterity wasn't installed")
+
 
 def test_suite():
     suite = unittest.TestSuite()
