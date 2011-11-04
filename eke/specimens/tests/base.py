23a11cad1ae8a69d0c679888e1dc1e1e55217d28
Sean Kelly <sean.c.kelly@me.com>
2011-11-04 01:33:32 +0000

    Update test data for simulated ERNE query

--- eke/specimens/tests/base.py
+++ eke/specimens/tests/base.py
@@ -52,7 +52,7 @@ _siteProtocolRDF = '''<?xml version='1.0' encoding='UTF-8'?>
 
 _protocolSpecimensRDF = ''''''
 
-_erneResponse = '''0\t3\t1\tA1$0\t3\t1\tA2$1\t3\t3\tC1$1\t3\t3\tC1$1\t18\t17\tC1$'''
+_erneResponse = '''0\t5\t1\tx@y.com\t1$1\t5\t2\tx@y.com\t1$1\t6\t3\tz@y.com\t1$'''
 
 def registerLocalTestData():
     ekeSiteBase.registerLocalTestData()
