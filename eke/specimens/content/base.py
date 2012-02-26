f3c4432d3b37e08158efb388bfce91028480c8bf
Sean Kelly <sean.c.kelly@me.com>
2012-02-26 23:17:05 +0000

    Add abstract bases "SpecimenSet", "ERNESpecimenSet"

--- eke/specimens/content/base.py
+++ eke/specimens/content/base.py
@@ -4,13 +4,17 @@
 
 '''EDRN Knowledge Environment Specimens: content base implementations and common features.'''
 
+from Acquisition import aq_inner, aq_parent
 from eke.specimens import ProjectMessageFactory as _
 from Products.Archetypes import atapi
 from zope.schema.interfaces import IVocabularyFactory
-from eke.specimens.interfaces import ISpecimenSet
+from eke.specimens.interfaces import ISpecimenSet, IERNESpecimenSet
 from zope.schema.vocabulary import SimpleVocabulary
 from zope.interface import directlyProvides
 from Products.CMFCore.utils import getToolByName
+from eke.specimens import STORAGE_VOCAB_NAME, COLLECTION_VOCAB_NAME
+from Products.ATContentTypes.content import folder
+from zope.interface import implements
 
 # FIXME: not i18n
 _diagnoses = SimpleVocabulary.fromItems((
@@ -18,21 +22,90 @@ _diagnoses = SimpleVocabulary.fromItems((
     ('Without Cancer', 'Without Cancer'),
 ))
 
-# Schema for ISpecimenStatistics: specimen count.
-CountsSchema = atapi.Schema((
-    atapi.IntegerField(
-        'specimenCount',
+StoredSpecimensSchema = atapi.Schema(( # Corresponds to eke.specimens.interfaces.IStoredSpecimens
+    atapi.StringField(
+        'storageType',
+        enforceVocabulary=True,
         required=False,
-        default=0,
-        searchable=False,
         storage=atapi.AnnotationStorage(),
-        widget=atapi.IntegerWidget(
-            label=_(u'Specimen Count'),
-            description=_(u'How many individual specimens have been collected.'),
-            visible={'edit': 'visible', 'view': 'visible'},
+        vocabulary_display_path_bound=-1,
+        vocabulary_factory=STORAGE_VOCAB_NAME,
+        widget=atapi.SelectionWidget(
+            label=_(u'Storage Type'),
+            description=_(u'In what form the specimen was processed and stored.'),
         ),
     ),
 ))
+TextuallyEnhancedSchema = atapi.Schema(( # Corresponds to eke.specimens.interfaces.ITextuallyEnhanced
+    atapi.TextField(
+        'text',
+        required=False,
+        searchable=True,
+        primary=True,
+        storage=atapi.AnnotationStorage(),
+        validators=('isTidyHtmlWithCleanup',),
+        default_output_type='text/x-html-safe',
+        widget=atapi.RichWidget(
+            label=_(u'Body Text'),
+            description=_(u'Additional richly-formatted text to display.'),
+            rows=10,
+            allow_file_upload=False,
+        ),
+    ),
+))
+SpecimenSetSchema = folder.ATFolderSchema.copy() + TextuallyEnhancedSchema.copy() + atapi.Schema((
+    atapi.ReferenceField(
+        'protocol',
+        storage=atapi.AnnotationStorage(),
+        enforceVocabulary=True,
+        multiValued=False,
+        vocabulary_factory=u'eke.study.ProtocolsVocabulary',
+        relationship='protocolProvidingSpecimens',
+        vocabulary_display_path_bound=-1,
+        widget=atapi.ReferenceWidget(
+            label=_(u'Protocol'),
+            description=_(u'The single protocol that guided collection of specimens in this set'),
+        ),
+    ),
+    atapi.ComputedField(
+        'systemName',
+        expression='context._computeSystemName()',
+        widget=atapi.ComputedWidget(
+            label=_(u'System'),
+            description=_(u'To what system this specimen set belongs.'),
+        ),
+    ),
+    atapi.IntegerField(
+        'totalNumSpecimens',
+        storage=atapi.AnnotationStorage(),
+        default=0,
+        required=False,
+        widget=atapi.IntegerWidget(
+            label=_(u'Total Specimens'),
+            description=_(u'Total number of specimens stored.'),
+        ),
+    ),
+))
+SpecimenSetSchema['title'].widget.label = _(u'Short Name')
+SpecimenSetSchema['title'].widget.description = _(u'Enter a short, unique identifier for this specimen set.')
+SpecimenSetSchema['title'].storage = atapi.AnnotationStorage()
+SpecimenSetSchema['description'].storage = atapi.AnnotationStorage()
+
+
+class SpecimenSet(folder.ATFolder):
+    '''Abstract set of specimens.  This is a base class.'''
+    implements(ISpecimenSet)
+    schema      = SpecimenSetSchema
+    title       = atapi.ATFieldProperty('title')
+    description = atapi.ATFieldProperty('description')
+    protocol    = atapi.ATReferenceFieldProperty('protocol')
+    text        = atapi.ATFieldProperty('text')
+    def _computeSystemName(self):
+        factory = getToolByName(self, 'portal_factory')
+        if factory.isTemporary(self): return u''
+        parent = aq_parent(aq_inner(self))
+        return parent.title
+
 
 def SpecimenCollectionNamesVocabularyFactory(context):
     catalog = getToolByName(context, 'portal_catalog')
@@ -53,3 +126,50 @@ def SitesWithSpecimensVocabulary(context):
     siteNames.sort()
     return SimpleVocabulary.fromItems([(i, i) for i in siteNames])
 directlyProvides(SitesWithSpecimensVocabulary, IVocabularyFactory)
+
+ERNESpecimenSetSchema = SpecimenSetSchema.copy() + atapi.Schema((
+    atapi.ReferenceField(
+        'site',
+        required=False,
+        searchable=False,
+        multiValued=False,
+        storage=atapi.AnnotationStorage(),
+        relationship='siteWhereTheseSpecimensAreCurated',
+        vocabulary_display_path_bound=-1,
+        vocabulary_factory=u'eke.site.Sites',
+        widget=atapi.ReferenceWidget(
+            label=_(u'Site'),
+            description=_(u'Site housing these specimens.'),
+        ),
+    ),
+    atapi.LinesField(
+        'organs',
+        required=False,
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.LinesWidget(
+            label=_(u'Organs'),
+            description=_(u'List (one per line) of the organs from which specimens were collected.'),
+        ),
+    ),
+    atapi.LinesField(
+        'collectionType',
+        required=False,
+        multiValued=False,
+        searchable=True,
+        storage=atapi.AnnotationStorage(),
+        vocabulary_display_path_bound=-1,
+        vocabulary_factory=COLLECTION_VOCAB_NAME,
+        widget=atapi.MultiSelectionWidget(  # TODO: Use InAndOutWidget
+            label=_(u'Collection Type'),
+            description=_(u'What kind of specimen was collected from participants.'),
+        ),
+    ),
+))
+
+class ERNESpecimenSet(SpecimenSet):
+    '''An abstract set of ERNE specimens.'''
+    implements(IERNESpecimenSet)
+    schema         = ERNESpecimenSetSchema
+    site           = atapi.ATReferenceFieldProperty('site')
+    organs         = atapi.ATFieldProperty('organs')
+    collectionType = atapi.ATFieldProperty('collectionType')
