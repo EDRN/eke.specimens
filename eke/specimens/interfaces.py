bfc890d6d9e1afdb62c9706f0b63d4d3c33134c0
Sean Kelly <sean.c.kelly@me.com>
2012-02-26 23:19:42 +0000

    Plenty of updates

--- eke/specimens/interfaces.py
+++ eke/specimens/interfaces.py
@@ -6,26 +6,64 @@
 '''
 
 from eke.site.interfaces import ISite
-from eke.specimens import ProjectMessageFactory as _
 from eke.study.interfaces import IProtocol
+
+from eke.specimens import ProjectMessageFactory as _
+
 from zope import schema
 from zope.app.container.constraints import contains
 from zope.interface import Interface
 
+class ICaseControlSubset(Interface):
+    '''A subset of participants who might be either disease-positive (case) or disease-negative (control).'''
+    title = schema.TextLine(
+        title=_(u'Title'),
+        description=_(u'The name of this case/control subset.'),
+        required=True,
+    )
+    description = schema.Text(
+        title=_(u'Description'),
+        description=_(u'An optional, short summary of this case/control subset.'),
+        required=False,
+    )
+    subsetType = schema.TextLine(
+        title=_(u'Subset Type'),
+        description=_(u'Either "Case" or "Control"'),
+        required=True,
+    )
+    def getNumParticipants():
+        '''Tells how many participants provided specimens in this subset.'''
+
 class ISpecimenStatistics(Interface):
     '''Contains specimen statistics, such as specimen count.'''
-    specimenCount = schema.Int(
-        title=_(u'Specimen Count'),
-        description=_(u'The number of specimens.'),
+    # specimenCount = schema.Int(
+    #     title=_(u'Specimen Count'),
+    #     description=_(u'The number of specimens.'),
+    #     required=False,
+    #     default=0,
+    #     min=0
+    # )
+    def getTotalNumSpecimens():
+        '''Tells the total number of specimens in this collection.'''
+
+class IStoredSpecimens(Interface):
+    '''A mix-in interface that provides a schema field for the specimen storage type.'''
+    def getStorageType():
+        '''Tell how specimens are stored.'''
+
+
+class ITextuallyEnhanced(Interface):
+    '''A mix-in interface that provides a schema field for additional rich text.'''
+    text = schema.Text(
+        title=_(u'Body Text'),
+        description=_(u'Additional richly-formatted text to display.'),
         required=False,
-        default=0,
-        min=0
     )
     
 
-class ISpecimenCollectionFolder(Interface):
-    '''Specimen collection folder.'''
-    contains('eke.specimens.interfaces.ISpecimenCollection')
+class ISpecimenSystemFolder(ITextuallyEnhanced):
+    '''Specimen system folder.'''
+    contains('eke.specimens.interfaces.ISpecimenSystem')
     title = schema.TextLine(
         title=_(u'Title'),
         description=_(u'Title of this folder.'),
@@ -36,19 +74,14 @@ class ISpecimenCollectionFolder(Interface):
         description=_(u'A short summary of this folder.'),
         required=False,
     )
-    text = schema.Text(
-        title=_(u'Body Text'),
-        description=_(u'Full body text to display on this folder above its contents.'),
-        required=False,
-    )
 
 
-class ISpecimenCollection(ISpecimenStatistics):
-    '''Specimen collection.'''
-    contains('eke.specimens.interfaces.ISpecimenSet')
+class ISpecimenSystem(ITextuallyEnhanced, ISpecimenStatistics):
+    '''Specimen system.'''
+    # contains('eke.specimens.interfaces.ISpecimenSet')
     title = schema.TextLine(
-        title=_(u'Title'),
-        description=_(u'Title of this collection.'),
+        title=_(u'Short Name'),
+        description=_(u'A short identifier of this specimen system.'),
         required=True,
     )
     description = schema.Text(
@@ -56,15 +89,12 @@ class ISpecimenCollection(ISpecimenStatistics):
         description=_(u'A short summary of this collection.'),
         required=False,
     )
-    text = schema.Text(
-        title=_(u'Body Text'),
-        description=_(u'Full body text to display on this collection above its contents.'),
-        required=False,
-    )
     
+
     
-class ISpecimenSet(ISpecimenStatistics):
-    '''Specimen set'''
+# class ISpecimenSet(ISpecimenStatistics):
+class ISpecimenSet(ITextuallyEnhanced, ISpecimenStatistics):
+    '''Abstract set of specimens'''
     title = schema.TextLine(
         title=_(u'Title'),
         description=_(u'Title of this folder'),
@@ -75,80 +105,150 @@ class ISpecimenSet(ISpecimenStatistics):
         description=_(u'A short summary of this folder.'),
         required=False,
     )
-    shortName = schema.TextLine(
-        title=_(u'Short Name'),
-        description=_(u'A brief name or simple identifier for this specimen set.'),
-        required=False,
-    )
-    storageType = schema.TextLine(
-        title=_(u'Storage Type'),
-        description=_(u'How the specimens were processed and stored from these participants.'),
-        required=True,
-    )
-    numberCases = schema.Int(
-        title=_(u'Cases'),
-        description=_(u'The number of participant cases from which specimens were drawn in this set.'),
-        required=True,
-        default=0,
-        min=0
-    )
-    numberControls = schema.Int(
-        title=_(u'Controls'),
-        description=_(u'The number of participant controls who provided specimens drawn in this set.'),
-        required=True,
-        default=0,
-        min=0
-    )
-    organs = schema.List(
-        title=_(u'Organs'),
-        description=_(u'Names of the organs from which specimens were taken.'),
-        required=False,
-        value_type=schema.TextLine(
-            title=_(u'Organ'),
-            description=_(u'Name of an organ from which specimens were taken.'),
-        )
-    )
-    diagnosis = schema.TextLine(
-        title=_(u'Diagnosis'),
-        description=_(u'Diagnosis of participants with or without cancer.'),
-        required=False,
-    )
+    # systemName: computed
     protocol = schema.Object(
         title=_(u'Protocol'),
         description=_(u'The single protocol that guided collection of specimens in this set.'),
         required=True,
         schema=IProtocol
     )
+
+    
+class IGenericSpecimenSet(ISpecimenSet, IStoredSpecimens):
+    '''A generic set of specimens, such as a reference set or a PRoBE set.'''
+    contains(
+        'eke.specimens.interfaces.ICaseControlSubset'
+        'Products.ATContentTypes.interfaces.IATFile',
+        'Products.ATContentTypes.interfaces.IATLink'
+    )
+    fullName = schema.TextLine(
+        title=_(u'Full Name'),
+        description=_(u'Complete title for this specimen set.'),
+        required=False,
+    )
+    cancerLocations = schema.List(
+        title=_(u'Cancer Locations'),
+        description=_(u'List (one per line) of the locations where cancer was detected.'),
+        required=False,
+        value_type=schema.TextLine(
+            title=_(u'Cancer Location'),
+            description=_(u'Location where cancer was detected.')
+        )
+    )
+    collectionType = schema.List(
+        title=_(u'Collection Types'),
+        description=_(u'What kinds of specimens were collected from participants.'),
+        required=False,
+        value_type=schema.TextLine(
+            title=_(u'Collection Type'),
+            description=_(u'What kind of specimen was collected from the participant.')
+        ),
+    )
+    def getNumParticipants():
+        '''Tell how many participants provided specimens in this set.'''
+    def getNumCases():
+        '''Tell how many participants were in the case subset.'''
+    def getNumControls():
+        '''Tell how many participants were in the control subset.'''
+
+class IInactiveStoredSpecimens(ISpecimenStatistics, IStoredSpecimens):
+    '''Specimens stored at an inactive or former ERNE site.'''
+    title = schema.TextLine(
+        title=_(u'Title'),
+        description=_(u'The name of this case/control subset.'),
+        required=True,
+    )
+    description = schema.Text(
+        title=_(u'Description'),
+        description=_(u'An optional, short summary of this case/control subset.'),
+        required=False,
+    )
+
+class IERNESpecimenSet(ISpecimenSet):
+    '''A set of specimens that comes from the EDRN Resource Network Exchange.'''
     site = schema.Object(
         title=_(u'Site'),
         description=_(u'Optional site at where these specimens are currently stored.'),
         required=False,
         schema=ISite
     )
-    siteName = schema.TextLine(
-        title=_(u'Site Name'),
-        description=_(u'Optional name of the site where these specimens are currently stored.'),
+    organs = schema.List(
+        title=_(u'Organs'),
+        description=_(u'Organs from which the specimens were sampled.'),
         required=False,
+        value_type=schema.TextLine(
+            title=_(u'Organ'),
+            description=_(u'Organ from which the specimen was sampled.')
+        )
     )
-    available = schema.Bool(
-        title=_(u'Available'),
-        description=_(u'Are the specimens in this set available for sharing?'),
-        required=False,
-        default=False,
+    collectionType = schema.TextLine(
+        title=_(u'Collection Type'),
+        description=_(u'What kind of specimen was collected from the participant.'),
+        required=True
     )
+
+class IInactiveERNESet(IERNESpecimenSet):
+    '''A set of specimens from a former ERNE site.'''
+    contains('eke.specimens.interfaces.IStoredInactiveSpecimen')
     contactName = schema.TextLine(
         title=_(u'Contact Name'),
         description=_(u'Name of the person to contact in order to obtain specimens from this set.'),
         required=False
     )
-    contactEmail = schema.TextLine(
-        title=_(u'Contact Email Address'),
-        description=_(u'Email address of the contact name.'),
+
+class IStoredInactiveSpecimen(IStoredSpecimens, ):
+    title = schema.TextLine(
+        title=_(u'Title'),
+        description=_(u'The name of these stored specimens.'),
+        required=True,
+    )
+    description = schema.Text(
+        title=_(u'Description'),
+        description=_(u'An optional summary or abstract of these stored specimens.'),
         required=False,
     )
-    isERNE = schema.Bool(
-        title=_(u'ERNE'),
-        description=_(u'Is this an ERNE specimen set?'),
+    totalNumSpecimens = schema.Int(
+        title=_(u'Number Stored'),
+        description=_(u'Total number of specimens stored of this type.'),
         required=False,
+        default=0,
+        min=0
     )
-    
+
+# class ActiveERNE
+# numParticipants = schema.Int(
+#     title=_(u'Number of Participants'),
+#     description=_(u'The total number of participants who provided specimens in this set.'),
+#     required=False,
+#     default=0,
+#     min=0
+# )
+
+
+    # diagnosis = schema.TextLine(
+    #     title=_(u'Diagnosis'),
+    #     description=_(u'Diagnosis of participants with or without cancer.'),
+    #     required=False,
+    # )
+    # siteName = schema.TextLine(
+    #     title=_(u'Site Name'),
+    #     description=_(u'Optional name of the site where these specimens are currently stored.'),
+    #     required=False,
+    # )
+    # available = schema.Bool(
+    #     title=_(u'Available'),
+    #     description=_(u'Are the specimens in this set available for sharing?'),
+    #     required=False,
+    #     default=False,
+    # )
+    # contactEmail = schema.TextLine(
+    #     title=_(u'Contact Email Address'),
+    #     description=_(u'Email address of the contact name.'),
+    #     required=False,
+    # )
+    # isERNE = schema.Bool(
+    #     title=_(u'ERNE'),
+    #     description=_(u'Is this an ERNE specimen set?'),
+    #     required=False,
+    # )
+    # 
