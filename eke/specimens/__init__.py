ffedf31e7d4ea6f777943dfb8c511db9800cbc94
Sean Kelly <sean.c.kelly@me.com>
2012-02-26 23:18:39 +0000

    Add collection vocab; update types

--- eke/specimens/__init__.py
+++ eke/specimens/__init__.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-# Copyright 2010 California Institute of Technology. ALL RIGHTS
+# Copyright 2010–2012 California Institute of Technology. ALL RIGHTS
 # RESERVED. U.S. Government Sponsorship acknowledged.
 
 '''EDRN Knowledge Environment Specimens: initialization.
@@ -9,8 +9,9 @@ from zope.i18nmessageid import MessageFactory
 
 ProjectMessageFactory = MessageFactory('eke.specimens')
 
-STORAGE_VOCAB_NAME = u'https://www.compass.fhcrc.org/edrns/cgi-bin/pilot/cde/CDEDetailed.asp?cdeid=529'
-ORGAN_VOCAB_NAME = u'https://www.compass.fhcrc.org/edrns/cgi-bin/pilot/cde/CDEDetailed.asp?cdeid=581'
+STORAGE_VOCAB_NAME    = u'https://www.compass.fhcrc.org/edrns/cgi-bin/pilot/cde/CDEDetailed.asp?cdeid=529'
+ORGAN_VOCAB_NAME      = u'https://www.compass.fhcrc.org/edrns/cgi-bin/pilot/cde/CDEDetailed.asp?cdeid=581'
+COLLECTION_VOCAB_NAME = u'https://www.compass.fhcrc.org/edrns/cgi-bin/pilot/cde/CDEDetailed.asp?cdeid=524'
 
 from eke.specimens import config
 from Products.Archetypes import atapi
@@ -18,7 +19,13 @@ import Products.CMFCore
 
 def initialize(context):
     '''Initializer called when used as a Zope 2 product.'''
-    from content import specimencollectionfolder, specimencollection, specimenset # for lame side effect
+    from content import (
+        specimensystemfolder,
+        specimensystem,
+        genericspecimenset,
+        casecontrolsubset,
+        inactiveerneset
+    ) # for lame side effect
     contentTypes, constructors, ftis = atapi.process_types(atapi.listTypes(config.PROJECTNAME), config.PROJECTNAME)
     for atype, constructor in zip(contentTypes, constructors):
         Products.CMFCore.utils.ContentInit(
