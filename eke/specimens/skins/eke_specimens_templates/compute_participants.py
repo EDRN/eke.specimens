8ba920c6a7263428a842785bbde94dc69471b06d
Sean Kelly <sean.c.kelly@me.com>
2013-04-11 15:29:46 +0000

    Fix permission problem when viewing sets who haven't updated their participant count in the catalog

--- eke/specimens/skins/eke_specimens_templates/compute_participants.py
+++ eke/specimens/skins/eke_specimens_templates/compute_participants.py
@@ -13,14 +13,6 @@ if item is None:
 
 value = item.getNumParticipants
 if not value:
-    # Is the value really zero?
     specimenSet = item.getObject()
-    actualValue = specimenSet.getNumParticipants()
-    if actualValue == 0:
-        # It really is zero. Wow.
-        return 0
-    # Ah ha. Reindex this individual specimen set so the "getNumParticipants" column gets fixed.
-    specimenSet.reindexObject()
-    return actualValue
-# Oh goodness gracious me, the column had a useful value!
+    return specimenSet.getNumParticipants()
 return value
