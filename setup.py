d7492c0b25e4b8cfc65376236e8bb0184c8d757a
Sean Kelly <sean.c.kelly@me.com>
2013-10-25 17:36:21 +0000

    bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import os.path
 # ------------
 
 _name        = 'eke.specimens'
-_version     = '1.1.8'
+_version     = '1.1.9'
 _description = 'ERNE Specimen Management for the EDRN Knowledge Environment'
 _url         = 'http://cancer.jpl.nasa.gov/products/eke-specimens'
 _downloadURL = 'http://oodt.jpl.nasa.gov/dist/eke'
